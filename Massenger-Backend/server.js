7f2c6a4a6bac26447ed77834f2b8d90121a3a49b
Satyam <satyambhardawaj@gmail.com>
2020-12-24 16:03:52 +0530

    working with mongodb

--- Massenger-Backend/server.js
+++ Massenger-Backend/server.js
@@ -1,8 +1,9 @@
 // import dependencies
-import express from 'express'
-import cors from 'cors'
-import Pusher from 'pusher'
-import mongoose from 'mongoose'
+const express = require('express')
+const cors = require('cors')
+const Pusher = require('pusher')
+const mongoose = require('mongoose')
+const mongoMessage = require('./messageModel')
 
 // app config
 const app = express()
@@ -12,11 +13,31 @@ const port = process.env.PORT || 9000
 
 
 //db config
+const uri = 'mongodb+srv://satty:Satyam1408@cluster0.2gapm.mongodb.net/messangerdb?retryWrites=true&w=majority'
+mongoose.connect(uri, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+})
 
+mongoose.connection.once('open', () => {
+    console.log('connected');
+})
 
 // api routes
 app.get('/', (req, res) => {
     res.status(200).send('Hello world')
+})
+app.post('/save/message', (req, res) => {
+    const dbMessage = req.body
+
+    mongoMessage.create(dbMessage, (error, data) => {
+        if (error) {
+            return res.status(500).send(error)
+        }
+
+        res.status(201).send(data)
+    })
 })
 
 //listen
